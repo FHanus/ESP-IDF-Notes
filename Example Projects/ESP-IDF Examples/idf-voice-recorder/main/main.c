600667419eef46c7b6bd82c324041e8c4169b7f0
Filip Hanus <filiphanus1@gmail.com>
2022-07-21 20:19:36 +0100

    update
    example

---
+++ Example Projects/ESP-IDF Examples/idf-voice-recorder/main/main.c
@@ -0,0 +1,202 @@
+/* HTTP File Server Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+#include <stdio.h>
+#include "sdkconfig.h"
+#include <string.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include <sys/unistd.h>
+#include <sys/stat.h>
+#include <sys/param.h>
+#include "soc/soc_caps.h"
+#include "sdmmc_cmd.h"
+
+#include "driver/sdmmc_host.h"
+#include "driver/i2s.h"
+
+#include "esp_event.h"
+#include "esp_log.h"
+#include "esp_system.h"
+#include "esp_spiffs.h"
+#include "esp_wifi.h"
+#include "esp_netif.h"
+#include "esp_err.h"
+#include "esp_vfs_fat.h"
+#include "nvs_flash.h"
+
+#include "protocol_examples_common.h"
+
+
+#define BYTE_RATE (44100 * (16 / 8))
+#define SAMPLE_SIZE (16 * 1024)
+
+static const char *TAG="example";
+static int16_t i2s_readraw_buff[SAMPLE_SIZE];
+const int rec_time = 5;
+const int WAVE_HEADER_SIZE = 44;
+size_t bytes_read;
+
+void generate_wav_header(char* wav_header, uint32_t wav_size, uint32_t sample_rate){
+
+    // See this for reference: http://soundfile.sapp.org/doc/WaveFormat/
+    uint32_t file_size = wav_size + WAVE_HEADER_SIZE - 8;
+    uint32_t byte_rate = BYTE_RATE;
+
+    const char set_wav_header[] = {
+        'R','I','F','F', // ChunkID
+        file_size, file_size >> 8, file_size >> 16, file_size >> 24, // ChunkSize
+        'W','A','V','E', // Format
+        'f','m','t',' ', // Subchunk1ID
+        0x10, 0x00, 0x00, 0x00, // Subchunk1Size (16 for PCM)
+        0x01, 0x00, // AudioFormat (1 for PCM)
+        0x01, 0x00, // NumChannels (1 channel)
+        sample_rate, sample_rate >> 8, sample_rate >> 16, sample_rate >> 24, // SampleRate
+        byte_rate, byte_rate >> 8, byte_rate >> 16, byte_rate >> 24, // ByteRate
+        0x02, 0x00, // BlockAlign
+        0x10, 0x00, // BitsPerSample (16 bits)
+        'd','a','t','a', // Subchunk2ID
+        wav_size, wav_size >> 8, wav_size >> 16, wav_size >> 24, // Subchunk2Size
+    };
+
+    memcpy(wav_header, set_wav_header, sizeof(set_wav_header));
+}
+
+void record_wav(void)
+{
+    ESP_LOGI(TAG, "Initializing SPIFFS");
+
+    esp_vfs_spiffs_conf_t conf = {
+      .base_path = "/spiffs",
+      .partition_label = NULL,
+      .max_files = 5,
+      .format_if_mount_failed = true
+    };
+
+    // Use settings defined above to initialize and mount SPIFFS filesystem.
+    // Note: esp_vfs_spiffs_register is an all-in-one convenience function.
+    esp_err_t ret = esp_vfs_spiffs_register(&conf);
+
+    if (ret != ESP_OK) {
+        if (ret == ESP_FAIL) {
+            ESP_LOGE(TAG, "Failed to mount or format filesystem");
+        } else if (ret == ESP_ERR_NOT_FOUND) {
+            ESP_LOGE(TAG, "Failed to find SPIFFS partition");
+        } else {
+            ESP_LOGE(TAG, "Failed to initialize SPIFFS (%s)", esp_err_to_name(ret));
+        }
+        return;
+    }
+
+    size_t total = 0, used = 0;
+    ret = esp_spiffs_info(conf.partition_label, &total, &used);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to get SPIFFS partition information (%s)", esp_err_to_name(ret));
+    } else {
+        ESP_LOGI(TAG, "Partition size: total: %d, used: %d", total, used);
+    }
+
+    
+    // Use POSIX and C standard library functions to work with files.
+    int flash_wr_size = 0;
+    ESP_LOGI(TAG, "Opening file");
+
+    char wav_header_fmt[WAVE_HEADER_SIZE];
+
+    uint32_t flash_rec_time = BYTE_RATE * rec_time;
+    generate_wav_header(wav_header_fmt, flash_rec_time, 44100);
+
+    // Create new WAV file "/spiffs/test.txt"  "/spiffs/record.wav", "wb"
+    FILE* f = fopen("/spiffs/recording.wav", "w");
+    if (f == NULL) {
+        ESP_LOGE(TAG, "Failed to open file for writing");
+        return;
+    }
+
+    // Write the header to the WAV file
+    fwrite(wav_header_fmt, 1, WAVE_HEADER_SIZE, f);
+
+    // Start recording
+    while (flash_wr_size < flash_rec_time) {
+        // Read the RAW samples from the microphone
+        i2s_read(0, (char *)i2s_readraw_buff, SAMPLE_SIZE, &bytes_read, 100);
+        // Write the samples to the WAV file
+        fwrite(i2s_readraw_buff, 1, bytes_read, f);
+        flash_wr_size += bytes_read;
+    }
+
+    ESP_LOGI(TAG, "Recording done!");
+    fclose(f);
+}
+
+void init_microphone(void)
+{
+    i2s_config_t i2s_config = {
+        .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
+        .sample_rate = 44100,
+        .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
+        .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
+        .communication_format = I2S_COMM_FORMAT_STAND_I2S,
+        .intr_alloc_flags = 0,
+        .dma_buf_count = 64,
+        .dma_buf_len = 1024,
+        .use_apll = 1
+    };
+
+    const i2s_pin_config_t pin_config = {
+        .bck_io_num = 18,
+        .ws_io_num = 5,
+        .data_out_num = -1,
+        .data_in_num = 4
+    };
+
+    // Call driver installation function before any I2S R/W operation.
+    ESP_ERROR_CHECK( i2s_driver_install(0, &i2s_config, 0, NULL) );
+    ESP_ERROR_CHECK( i2s_set_pin(0, &pin_config) );
+    ESP_ERROR_CHECK( i2s_set_clk(0, 44100, I2S_BITS_PER_SAMPLE_16BIT, I2S_CHANNEL_MONO) );
+}
+
+/* Declare the function which starts the file server.
+ * Implementation of this function is to be found in
+ * file_server.c */
+esp_err_t start_file_server(const char *base_path);
+
+void app_main(void)
+{
+    ESP_LOGI(TAG, "PDM microphone recording Example start");
+    // Mount the SDCard for recording the audio file
+    //mount_sdcard();
+    // Init the PDM digital microphone
+    init_microphone();
+    ESP_LOGI(TAG, "Starting recording for %d seconds!", rec_time);
+    // Start Recording
+    record_wav();
+    // Stop I2S driver and destroy
+    ESP_ERROR_CHECK( i2s_driver_uninstall(0) );
+
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+     * Read "Establishing Wi-Fi or Ethernet Connection" section in
+     * examples/protocols/README.md for more information about this function.
+     */
+    ESP_ERROR_CHECK(example_connect());
+
+    /* Initialize file storage */
+
+    //ESP_ERROR_CHECK(init_spiffs());
+
+    /* Start the file server */
+    ESP_ERROR_CHECK(start_file_server("/spiffs"));
+
+}
